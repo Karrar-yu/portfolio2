94b59aa2f854059976bb686a4ba8d97f6f1a8a2b
“SashaYuu” <krarmohamed65@gmail.com>
2024-03-06 12:01:08 +0100

    added a mini game

--- src/js/main.js
+++ src/js/main.js
@@ -77,3 +77,31 @@ window.addEventListener('resize', function() {
   
   // Fire the event once on page load to initialize the state based on the screen size
   window.dispatchEvent(new Event('resize'));
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("openModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
